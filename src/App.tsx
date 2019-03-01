aae889f790e7ad884382c89663256ec17ae812c7
Kevin Stout <Stoutkev3@gmail.com>
2019-03-01 09:32:43 -0600

    added Header and Description component also some folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import * as React from "react";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import Header from "./components/Header";
+import Description from "./components/Description";
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
+      <div className="messingAround">
+        <Header/>
+      </div>
+        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
+          <Description/>
           <a
             className="App-link"
             href="https://reactjs.org"
